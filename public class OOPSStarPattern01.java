61a0b95354f5e3746cdd70dcfb524a295d47fd87
Chandrgupt08 <cs0258@srmist.edu.in>
2026-02-16 09:00:42 +0530

    Initial ommit

---
+++ public class OOPSStarPattern01.java
@@ -0,0 +1,38 @@
+public class OOPSStarPattern01 {
+    public static void main(String[] args) {
+
+        // O
+        System.out.println(" *****   ");
+        System.out.println("*     *  ");
+        System.out.println("*     *  ");
+        System.out.println("*     *  ");
+        System.out.println(" *****   ");
+
+        System.out.println();
+
+        // O
+        System.out.println(" *****   ");
+        System.out.println("*     *  ");
+        System.out.println("*     *  ");
+        System.out.println("*     *  ");
+        System.out.println(" *****   ");
+
+        System.out.println();
+
+        // P
+        System.out.println(" *****   ");
+        System.out.println("*     *  ");
+        System.out.println(" *****   ");
+        System.out.println("*        ");
+        System.out.println("*        ");
+
+        System.out.println();
+
+        // S
+        System.out.println(" *****   ");
+        System.out.println("*        ");
+        System.out.println(" *****   ");
+        System.out.println("      *  ");
+        System.out.println(" *****   ");
+    }
+}
