ad0e8806edb034a12b950547d0d6acbe7f138ff7
Chandrgupt008 <chandraguptsrivastav@gmail.com>
2026-03-18 10:10:44 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,13 @@
+/**
+ * UC1 - Print the literal text "OOPS" to the console.
+ * This program runs directly inside VS Code.
+ */
 public class OOPSBannerApp {
-public static void main(String[] args) {
-System.out.println("00PS");
 
-}
+    public static void main(String[] args) {
 
+        // Print OOPS
+        System.out.println("OOPS");
+
+    }
 }
